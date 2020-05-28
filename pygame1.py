9bf829fdc5d7bbb314f7a99149956f0c43534e68
Juan Parra <parralopera755@gmail.com>
2020-05-28 17:57:39 -0500

    Se dibuja por medio de los forloop de esta manera evitar sobrecargar codigo

--- pygame1.py
+++ pygame1.py
@@ -23,9 +23,10 @@ while True:
     #metodo para poner color de fondo
     screen.fill(WHITE)
     ####----zona de dibujo
-    pygame.draw.line(screen, GREEN, [0, 100], [200, 300], 5)
-    pygame.draw.rect(screen, BLACK, (100, 100, 80,80))
-    pygame.draw.circle(screen, BLACK, (200,200), 30)
+    #forloop (100 es donde voy a iniciar en pixeles 700 a donde quiero ir y los ultimos 100 el numero de cada incremento 100 en 100)
+    for x in range(100, 700, 100):
+        pygame.draw.rect(screen, BLACK, (x, 50, 50, 50))
+        pygame.draw.line(screen, GREEN, (x, 0,),(x, 100), 5)
 
     ####-----zona de dibujo
     #metodo para actualizar pantalla
