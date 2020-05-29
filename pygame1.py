6fa78a17b437cd1905efa67c80e2c6b06c5975da
Juan Parra <parralopera755@gmail.com>
2020-05-29 12:30:36 -0500

    se agrego una animacion jugando con las coordenadas y una variable de velocidad

--- pygame1.py
+++ pygame1.py
@@ -14,23 +14,39 @@ size = (800, 500)
 #crear ventana
 screen = pygame.display.set_mode(size)
 
+#controlar FPS
+clock = pygame.time.Clock()
+
+#coordenadas
+cord_x = 400
+cord_y = 200
+
+#velocidad a la que se movera mi cuadrado
+speed_x = 3
+speed_y = 3
 
 while True:
     for event in pygame.event.get():
         print(event)
         if event.type == pygame.QUIT:
             sys.exit()
+
+    if(cord_x > 720 or cord_x < 0):
+        speed_x *= -1
+    if(cord_y > 400 or cord_y < 0):
+        speed_y *= -1
+    cord_x += speed_x
+    cord_y += speed_y
     #metodo para poner color de fondo
     screen.fill(WHITE)
     ####----zona de dibujo
     #forloop (100 es donde voy a iniciar en pixeles 700 a donde quiero ir y los ultimos 100 el numero de cada incremento 100 en 100)
-    for x in range(100, 700, 100):
-        pygame.draw.rect(screen, BLACK, (x, 50, 50, 50))
-        pygame.draw.line(screen, GREEN, (x, 0,),(x, 100), 5)
+    pygame.draw.rect(screen, RED, (cord_x, cord_y, 80, 80))
 
     ####-----zona de dibujo
     #metodo para actualizar pantalla
     pygame.display.flip()
+    clock.tick(60)
 
 
 
